b6474aba8b931e590dbc66ec526f04dec8d7847a
Ultron <40607807+MarkUltraV1@users.noreply.github.com>
2018-06-27 07:27:58 -0600

    cambios en el employee

--- src/Entity/Employee.java
+++ src/Entity/Employee.java
@@ -12,5 +12,34 @@ package Entity;
 public class Employee {
     Long id;
     Person person;
+
+    public Employee() {
+    }
+
+    public Employee(Long id, Person person) {
+        this.id = id;
+        this.person = person;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Person getPerson() {
+        return person;
+    }
+
+    public void setPerson(Person person) {
+        this.person = person;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" + "id=" + id + ", person=" + person + '}';
+    }
     
 }
