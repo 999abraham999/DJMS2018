fa0b572c5bc76c2556f71716699bd040115e0270
Ultron <40607807+MarkUltraV1@users.noreply.github.com>
2018-07-02 22:05:09 -0600

    se añadio la conexion a sqlite

--- src/ConnectionDB/ConnectionSQLite.java
+++ src/ConnectionDB/ConnectionSQLite.java
@@ -4,11 +4,24 @@
  * and open the template in the editor.
  */
 package ConnectionDB;
-
+ 
+import java.sql.*;
+import javax.swing.JOptionPane;
 /**
  *
  * @author ABRAHAM
  */
 public class ConnectionSQLite {
+    Connection conn = null;
     
+    public static Connection ConnectDB(){
+        try {
+            Class.forName("org.sqlite.JDBC");
+            Connection conn = DriverManager.getConnection("jdbc:sqlite:DJMI_DB.sqlite");
+            JOptionPane.showMessageDialog(null, "Connected");
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "Error en la conexion");
+        }
+    return null;
+    }
 }
