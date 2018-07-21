f425cb23d144aee266f7ac21d2bb54071a0c8455
Ultron <40607807+MarkUltraV1@users.noreply.github.com>
2018-07-20 20:46:15 -0600

    new commit

--- src/Sell/SellRepository.java
+++ src/Sell/SellRepository.java
@@ -8,11 +8,9 @@ package Sell;
 import ConnectionDB.ConnectionSQLite;
 import Entity.Product;
 import java.sql.Connection;
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import org.omg.CORBA.Any;
 
 /**
  *
